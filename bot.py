f25c5d63d6b532a1e6740643bcaa017aad583133
Anas Saeed <saeedanas396@gmail.com>
2021-04-19 20:45:38 -0700

    ISS-bot with spreadsheet features v1

--- bot.py
+++ bot.py
@@ -1,42 +1,72 @@
-import discord
-from discord.ext import commands
 import os
 
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+
+from spreadsheet import StrikeSheet
 
 load_dotenv('.env')
 
+TOKEN = os.getenv('TOKEN')
 
-client = discord.Client()
-client=commands.Bot(command_prefix = '!')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    names = ['aditya','anas','anokhi','anuj','harshil','krish','naman','neil','poorvi','preet','ray','shanjay','shreyes','sreshta','srinidhi']
 
-    if message.content.startswith('!strike'):
-        name = message.content.split(' ')[1]
-        if name.lower() in names:
-            split_version = message.content.split(' - ')
-            if len(split_version) == 2:
-                await message.channel.send(name + ' is striked for ' + split_version[1])
-            else:
-                await message.channel.send('Please write it in the format: !strike name - reason')
-        else:
-            await message.channel.send(name + ' is not registered')
-            await message.channel.send('Please write it in the format: !strike name - reason')
-    elif message.content.startswith('!unstrike'):
-        Name = message.content.split(' ')[1]
-        await message.channel.send(Name + ' is unstriked')
-    elif message.content.startswith('!spreadsheet'):
-        await message.channel.send('https://docs.google.com/spreadsheets/d/1DVPDeYkLLwjkL2BTBlBRUWsmnpew-z5fjT9T9wMYri0/edit?usp=sharing')
+async def is_admin(ctx):
+    roles = list(filter(lambda role: role.name == 'Admin', ctx.author.roles))
+    return len(roles) > 0
 
-client.run(os.getenv('TOKEN'))
+
+@bot.command(name='registerall', help='logs all users onto spreadsheet')
+@commands.check(is_admin)
+async def init_sheet(ctx):
+    members = ctx.guild.members
+    s = StrikeSheet(ctx)
+    await s.register_members(members)
+
+
+@bot.command(name='register', help='register new member')
+@commands.check(is_admin)
+async def register(ctx, uid):
+    members = ctx.guild.members
+    member = filter(lambda m: f'<@!{m.id}>' == uid, members)
+    s = StrikeSheet(ctx)
+    await s.register_member(member[0])
+
+
+@bot.command(name='strike', help='Strikes a user')
+@commands.check(is_admin)
+async def strike(ctx, *args):
+    user = args[0]
+    reason = ' '.join(args[1:])
+    s = StrikeSheet(ctx)
+    try:
+        await s.strike_user(user, reason)
+    except Exception:
+        await ctx.send('Please enter a valid user id')
+
+
+@bot.command(name='unstrike', help='Strikes a user')
+@commands.check(is_admin)
+async def unstrike(ctx, uid):
+    s = StrikeSheet(ctx)
+    try:
+        await s.remove_strike(uid)
+    except Exception:
+        await ctx.send('Please enter a valid user id')
+
+
+@bot.command(name='spreadsheet', help='Gives link to strikesheet')
+async def spreadsheet(ctx):
+    s = StrikeSheet(ctx)
+    await s.spreadsheet()
+
+
+bot.run(TOKEN)
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,9 +1,76 @@
 import gspread
 
-gc = gspread.service_account(filename='./service_account.json')
+# strike tracker link: https://docs.google.com/spreadsheets/d/1DVPDeYkLLwjkL2BTBlBRUWsmnpew-z5fjT9T9wMYri0/edit#gid=0
 
-sh = gc.open("Strike Tracker")
+strike_col = 3
 
-worksheet = sh.sheet1
 
-worksheet.update_cell(2, 3, '')
+def get_sheet(name):
+    gc = gspread.service_account(filename='./service_account.json')
+    sh = gc.open(name)
+    return sh.sheet1
+
+
+class StrikeSheet:
+    strike_col = 3
+    reason_col = 4
+    sheet = 'https://docs.google.com/spreadsheets/d/1DVPDeYkLLwjkL2BTBlBRUWsmnpew-z5fjT9T9wMYri0/edit#gid=0'
+
+    def __init__(self, ctx):
+        self.ws = get_sheet("Strike Tracker")
+        self.ctx = ctx
+
+    async def register_member(self, member):
+        if not member.bot:
+            name = member.display_name.split(' ')[0]
+            uid = f'<@!{member.id}>'
+
+            try:
+                self.find_user(uid)
+                await self.ctx.send(f'{name} is already registered!')
+            except Exception:
+                self.ws.append_row([name, uid, 0])
+                await self.ctx.send(f'{name} has been registered')
+
+    async def register_members(self, members):
+        for member in members:
+            await self.register_member(member)
+
+    def find_user(self, uid):
+        return self.ws.find(uid).row
+
+    def get_strikes(self, user):
+        return int(self.ws.cell(user, strike_col).value)
+
+    def update_strikes(self, user, value):
+        return self.ws.update_cell(user, strike_col, value)
+
+    def get_reason(self, user, strike):
+        return self.ws.cell(user, self.reason_col + strike - 1).value
+
+    def update_reason(self, user, strike, reason):
+        return self.ws.update_cell(user, self.reason_col + strike - 1, reason)
+
+    async def strike_user(self, uid, reason):
+        user = self.find_user(uid)
+        strikes = self.get_strikes(user) + 1
+        if strikes >= 3:
+            await self.ctx.send(
+                f"{uid} has been striked 3 times! His fate is now under Krish's control"
+            )
+            return
+        self.update_strikes(user, strikes)
+        self.update_reason(user, strikes, reason)
+        await self.ctx.send(f'User: {uid} is striked because: {reason}')
+
+    async def remove_strike(self, uid):
+        user = self.find_user(uid)
+        strikes = self.get_strikes(user)
+        if strikes <= 1:
+            await self.ctx.send(f'{uid} has no strikes...')
+            return
+        self.update_strikes(user, strikes - 1)
+        self.update_reason(user, strikes, '')
+
+    async def spreadsheet(self):
+        await self.ctx.send(f'{self.sheet}')
